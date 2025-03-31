cf04647ca710754ad7d62d08fd9f99c7552be210
Andjela <andela.dermanovic@merkle.com>
2025-03-31 13:52:19 +0200

    #3103 - clean storybook files

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 
-import './button.css';
+import './button.scss';
 
 export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
   primary?: boolean;
-  /** What background color to use */
   backgroundColor?: string;
-  /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
-  /** Button contents */
   label: string;
-  /** Optional click handler */
   onClick?: () => void;
 }
 
-/** Primary UI component for user interaction */
 export const Button = ({
   primary = false,
   size = 'medium',
@@ -27,7 +21,7 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={`storybook-button storybook-button--${size} ${mode}`}
       style={{ backgroundColor }}
       {...props}
     >
